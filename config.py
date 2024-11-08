dc3bd1762b0197b573ebe13d2124741aa482af74
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-11-08 12:31:45 +0530

    Update config.py
    Signed-off-by: Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.
 
 SESSION = getenv("BQGlNysAkOQdMB3BJCmjA_kFhkQG3aJCIyovWpXK4s3qcrLuC6U0NyZsFnE5RldbhNyP3txhY0-WqfzUZ1aPoMjhS18jYHTdI3ihQ78wn31q8gE6lwXl6yCYzTQphpFiU998Jbt-bNLxJiYgJvpUdbaiboDV7mnap2P8ciQ_pvIIEc2QpuVy0iVd7NZTp6ofUr2uvorJc41qoQZFo9-BFeeZbXXRS7lQPIefmGxWCSzCewNcJzfpG3-HhJTSGfhrQVrpEYOsS7zcuLI2Hcp_ys6TJpQlJTh1CTH9dqmK5RODWMT8IbwQaNnO-q7lyWsG5cA5-ParRSan8qKMjcemAcUKSVT2lAAAAAG2bCpvAA", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/land_of_hunters")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/land_of_hunters")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
