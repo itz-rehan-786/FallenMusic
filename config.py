bc5d2e60edde9c626660841dd5acf9421f557201
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-11-08 13:08:59 +0530

    Update config.py
    Signed-off-by: Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("8066514303:AAHaQOjQYGa-lPCDJ0hLl-assumizp80U9E", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("7775259302"))
+OWNER_ID = getenv("7775259302")
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
